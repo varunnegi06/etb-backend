4de0e6857db01848b00254729bd893954b2d056a
Varun Negi <varun.a.negi@accenture.com>
2020-12-12 13:58:10 +0530

    added routes

--- routes/login/login.js
+++ routes/login/login.js
@@ -6,5 +6,9 @@ routes.get("/login",async function(req,res){
     res.send({ "message":"login Successfully" });
 });
 
+routes.post("/loginpost",async function(req,res){
+    console.log(JSON.stringify(req.body));
+    res.send({ "message":"login Successfully" });
+});
 
 module.exports= routes;
