c9537e13b5c68dffe127a182635698a301571ffa
Varun Negi <varun.a.negi@accenture.com>
2020-12-13 13:27:34 +0530

    added register js

---
+++ routes/register.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var routes = express.Router();
+const { User} = require('../db/sequalize');
+
+routes.get('/api/users', async (req, res) => {
+    let result = await User.findAll();
+    console.log(JSON.stringify(result));
+    res.json(result)
+})
+
+routes.post("/loginpost",async function(req,res){
+    console.log(JSON.stringify(req.body));
+    res.send({ "message":"login Successfully" });
+});
+
+module.exports= routes;
